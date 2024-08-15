0e543182f63de4ef189ad6331986b0175855c36d
Maryna-Korbet <m22v86k@gmail.com>
2024-08-14 23:01:00 -0400

    configured the backend

--- backend/movierater/settings.py
+++ backend/movierater/settings.py
@@ -41,9 +41,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
